ee6be1c545e7a383976b8d8add81279390ba38a1
abuabdullah23 <abuobaidazayed@gmail.com>
2023-06-01 10:32:42 +0600

    delete method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,9 +28,17 @@ async function run() {
     await client.connect();
 
     // data get from DB
-    app.get('/coffee', async(req, res)=>{
+    app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // get single data from db after delete/post/update
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
       res.send(result);
     })
 
@@ -38,10 +46,18 @@ async function run() {
     const coffeeCollection = client.db('coffeeDb').collection('coffee');
 
     // add one item in server
-    app.post('/coffee', async(req, res)=>{
+    app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    // delete item
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -57,11 +73,11 @@ run().catch(console.dir);
 
 // =================================
 
-app.get('/', (req, res)=>{
-    res.send('Coffee making server is running')
+app.get('/', (req, res) => {
+  res.send('Coffee making server is running')
 })
 
 
-app.listen(port, ()=>{
-    console.log(`Coffee server is running on port : ${port}`)
+app.listen(port, () => {
+  console.log(`Coffee server is running on port : ${port}`)
 })
